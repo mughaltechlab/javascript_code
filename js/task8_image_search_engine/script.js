178a1dc0d4d26c1b497e2f77b50423e14da7caff
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-03-23 06:50:03 +0500

    image search engine complete

---
+++ js/task8_image_search_engine/script.js
@@ -0,0 +1,57 @@
+const apiKey = "OD3wGJ-WhSyEd7wZMpMe8JdDHXIvIj8Y5oNw693ZGqw";
+const searchForm = document.getElementById('searchForm');
+const searchBox = document.getElementById('searchBox');
+const searchResult = document.getElementById('searchResult');
+const showMoreBtn = document.getElementById('showMoreBtn');
+
+
+
+let page = 1;
+let keyword = "";
+
+
+searchForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    page=1;
+    fetchData();
+})
+
+
+async function fetchData(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${apiKey}&per_page=12`;
+
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    const results = data.results;
+
+    results.map((result)=>{
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        
+        searchResult.appendChild(imageLink);
+    });
+
+    showMoreBtn.style.display = 'block';
+
+    console.log(data);
+
+}
+
+showMoreBtn.addEventListener('click',()=>{
+    page++;
+    fetchData();
+})
+
+
+
+
+// fetch("").then(async(resp)=>{
+//     const data = await resp.json();
+// })
